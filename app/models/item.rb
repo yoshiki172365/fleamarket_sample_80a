ef7b374ea8795ff2928dcba06dbfc03ecf908d1a
natty-mii <n.miya7@gmail.com>
2020-10-26 16:27:45 +0900

    add enum

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,8 @@ class Item < ApplicationRecord
   validates :prefecture, presence: true
   validates :prefecture_id, numericality: { other_than: 0 }
 
-  
+  enum condition: [:condition_sentaku, :shinpin, :hobomishi, :kizunahi, :yayakizuari, :kizuari]
+  enum delivery_fee: [:delivery_fee_sentaku, :seller, :buyer]
+  enum shipping_day: [:shipping_day_sentaku, :until_two_days, :until_three_days, :until_seven_days]
+
 end
