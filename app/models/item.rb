884f33d756336605761383279147de824789d416
natty-mii <n.miya7@gmail.com>
2020-10-26 15:47:31 +0900

    add active_hash and create model item

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  validates :prefecture, presence: true
+  validates :prefecture_id, numericality: { other_than: 0 }
+
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_112623) do
+ActiveRecord::Schema.define(version: 2020_10_26_062827) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "zip_code", null: false
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2020_10_08_112623) do
     t.string "block_number", null: false
     t.string "apartment_name"
     t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
