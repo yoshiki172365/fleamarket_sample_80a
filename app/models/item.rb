9b86a715ea57e19501d8dd825388b7352db7277a
keitaroinoue6 <68646591+keitaroinoue6@users.noreply.github.com>
2020-10-28 21:26:18 +0900

    Revert "[WIP]商品詳細ページ"

--- app/models/item.rb
+++
@@ -1,4 +0,0 @@
-class Item < ApplicationRecord
-  def show
-  end
-end
--- db/migrate/20201012110107_create_cards.rb
+++
@@ -1,9 +0,0 @@
-class CreateCards < ActiveRecord::Migration[6.0]
-  def change
-    create_table :cards do |t|
-      t.references :user, foreign_key: true
-      t.string :customer_id, null: false
-      t.timestamps
-    end
-  end
-end
