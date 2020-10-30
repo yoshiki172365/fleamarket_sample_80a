ce4d4d36a10a1461e8a1ce4b1eb6c46c61462626
natty-mii <n.miya7@gmail.com>
2020-10-31 00:29:26 +0900

    fixed typo

--- db/migrate/20201026062827_create_items.rb
+++ db/migrate/20201026062827_create_items.rb
@@ -11,7 +11,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer       :delivery_date_id, null: false
       t.integer       :price, null: false
       t.integer       :trading_status, null: false
-      t.refereces     :user, null: false, foreign_key: true
+      t.references     :user, null: false, foreign_key: true
 
       t.timestamps
     end
