9980a3307554abb7299e598011ad442288273ed6
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2020-11-02 17:13:55 +0900

    マイグレーションファイル修正

--- db/migrate/20201008112623_create_addresses.rb
+++ db/migrate/20201008112623_create_addresses.rb
@@ -1,14 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.string      :zip_code, null: false
-      t.integer     :prefecture_id, null: false
-      t.string      :city, null: false
-      t.string      :block_number, null: false
-      t.string      :apartment_name
-      t.integer     :user_id, null: false
-
+      t.string :zip_code, null: false
+      t.string :prefectures, null: false
+      t.string :city, null: false
+      t.string :block_number, null: false
+      t.string :apartment_name
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
 end
+
