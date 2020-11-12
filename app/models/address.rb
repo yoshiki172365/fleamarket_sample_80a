c37fd212169e00ac0cb2aef4e391789c5da00b92
keitaroinoue6 <k25k.soccer@ezweb.ne.jp>
2020-11-12 11:55:39 +0900

    新規登録

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,7 @@
 class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  validates :prefecture, presence: true
-  validates :prefecture_id, numericality: { other_than: 0 }
+  belongs_to :user, optional: true
+  validates :prefectures, presence: true
+  validates :prefectures, numericality: { other_than: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,5 @@ class User < ApplicationRecord
   format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
   validates :last_name_kana,
   format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
+  has_one :address
 end
