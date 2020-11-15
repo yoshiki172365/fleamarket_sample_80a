131256b223a99053bf6f20f5ae54740b99c36d53
natty-mii <n.miya7@gmail.com>
2020-11-15 23:05:22 +0900

    update routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
-         ,password_length: 7..128
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2], password_length: 7..128
 
   has_many :sns_credentials
 
