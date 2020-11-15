135ebfeb3d6f571018b134aa53943132fe99ad7a
natty-mii <n.miya7@gmail.com>
2020-11-15 23:15:30 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,20 @@ class User < ApplicationRecord
   has_one :address
   has_one :card
   has_many :items
+
+  def self.from_omniauth(auth)
+  sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+  # sns認証したことがあればアソシエーションで取得
+  # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+  user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+    nickname: auth.info.name,
+      email: auth.info.email
+  )
+  # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+  if user.persisted?
+    sns.user = user
+    sns.save
+  end
+  user
+  
 end
