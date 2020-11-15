29f86cdc295fa36b1a1d179efac0d177ce910d66
natty-mii <n.miya7@gmail.com>
2020-11-15 22:51:18 +0900

    generate sns_credentials.rb and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_144514) do
+ActiveRecord::Schema.define(version: 2020_11_15_134811) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -80,6 +80,15 @@ ActiveRecord::Schema.define(version: 2020_11_04_144514) do
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
@@ -109,4 +118,5 @@ ActiveRecord::Schema.define(version: 2020_11_04_144514) do
   add_foreign_key "items", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
+  add_foreign_key "sns_credentials", "users"
 end
