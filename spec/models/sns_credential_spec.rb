13a893286982293b268b126a57e0ab30d1a593ed
natty-mii <n.miya7@gmail.com>
2020-11-16 10:34:07 +0900

    add rspec test sns credential

--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
-RSpec.describe SnsCredential, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  describe SnsCredential do
+    describe '#facebook' do
+      it "providerがない場合は登録できないこと" do
+        sns_credential = build(:sns_credential, provider: "")
+        sns_credential.valid?
+        expect(sns_credential.errors[:provider]).to include("を入力してください")
+      end
+
+      it "uidがない場合は登録できないこと" do
+        sns_credential = build(:sns_credential, uid: "")
+        sns_credential.valid?
+        expect(sns_credential.errors[:uid]).to include("を入力してください")
+      end
+
+      it "provider, uidが存在すれば登録出来ること" do
+        sns_credential = build(:sns_credential)
+        expect(sns_credential).to be_valid
+      end
+    end
+
+    describe '#google_oauth2' do
+      it "providerがない場合は登録できないこと" do
+        sns_credential = build(:sns_credential, provider: "")
+        sns_credential.valid?
+        expect(sns_credential.errors[:provider]).to include("を入力してください")
+      end
+
+      it "uidがない場合は登録できないこと" do
+        sns_credential = build(:sns_credential, uid: "")
+        sns_credential.valid?
+        expect(sns_credential.errors[:uid]).to include("を入力してください")
+      end
+
+      it "provider, uidが存在すれば登録出来ること" do
+        sns_credential = build(:sns_credential)
+        expect(sns_credential).to be_valid
+      end
+    end
+
+  end
