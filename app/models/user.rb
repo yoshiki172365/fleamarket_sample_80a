17759b3c17023e88ece09963a0610e25fd5d6611
natty-mii <n.miya7@gmail.com>
2020-12-11 13:13:26 +0900

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,19 +20,6 @@ class User < ApplicationRecord
   belongs_to_active_hash :birth_day
   validates :birth_day_id, numericality: { other_than: 0 }
 
-
-  validates :nickname, :birth_day, :first_name, :last_name, :first_name_kana, :last_name_kana, :encrypted_password, presence: true
-  validates :email, uniqueness: true,
-    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-
-  validates :first_name,
-    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
-  validates :last_name,
-    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'を全角で入力してください' }
-  validates :first_name_kana,
-    format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
-  validates :last_name_kana,
-    format: { with: /\A([ァ-ン]|ー)+\z/, message: 'を全角カタカナで入力してください' }
   has_one :address
   has_one :card
   has_many :items
@@ -49,6 +36,9 @@ class User < ApplicationRecord
 
   validates :email,
   format: { with: /\A[\x21-\x3f\x41-\x7e]+@(?:[-a-z0-9]+\.)+[a-z]{2,}\z/i, message: 'を正しいフォーマットで入力してください'}
+
+  validates :password,
+  format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,128}/, message: 'を正しいフォーマットで入力してください'}
 
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
